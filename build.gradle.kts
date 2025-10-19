3682a02080ce58e02caa9a3f23b51deeca8f50e1
dinhmyhuyenalho91050-boop <dinhmyhuyenalho91050@gmail.com>
2025-10-19 20:20:46 +0800

    Delete build.gradle.kts

--- build.gradle.kts
+++
@@ -1,7 +0,0 @@
-plugins {
-    id("com.android.application") version "8.10.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.0" apply false
-    id("com.google.devtools.ksp") version "2.2.0-2.0.2" apply false
-}
